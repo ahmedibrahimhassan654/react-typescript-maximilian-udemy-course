0dafc0aeaec0f2eb9a87ecfce646e8c177209ac9
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-03-07 20:34:12 +0200

    12 - Combining Types Union Types Alternative Types

---
+++ app.ts
@@ -0,0 +1,16 @@
+let userName:string
+
+// userName=10
+
+userName = 'Max';
+
+let userAge = 34;
+
+let isValid = true;
+
+// string, number, boolean
+
+let userID:string|number
+
+userID="ACD3$t"
+userID=5
